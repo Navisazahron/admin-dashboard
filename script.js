11a72727d2ecfc921cbdaf21fc5046d5eb9b4df1
navisa <zahronnafis.com>
2024-07-11 11:54:14 +0700

    first

---
+++ script.js
@@ -0,0 +1,25 @@
+const menuOpen = document.getElementById('menu-open');
+const menuClose = document.getElementById('menu-close');
+const sideBar = document.querySelector('.container .left-section');
+const sideBarItems = document.querySelectorAll('.container .left-section .sidebar .item');
+
+menuOpen.addEventListener('click', () => {
+    sideBar.style.top = '0';
+});
+
+menuClose.addEventListener('click', () => {
+    sideBar.style.top = '-60vh';
+});
+
+let activeItem = sideBarItems[0];
+
+sideBarItems.forEach(item => {
+    item.addEventListener('click', () => {
+        if (activeItem) {
+            activeItem.removeAttribute('id');
+        }
+
+        item.setAttribute('id', 'active');
+        activeItem = item;
+    });
+});
